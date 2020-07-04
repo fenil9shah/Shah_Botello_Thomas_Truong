f71709f779466f668fa705532cfe424ed72e35c5
jbthoma4 <62680688+jbthoma4@users.noreply.github.com>
2020-07-04 04:25:22 -0500

    added the pricing module class

--- htdoc files/fuel_quote.php
+++ htdoc files/fuel_quote.php
@@ -1,3 +1,57 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "root", "test");
+    if(!$con)
+    {
+        die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+    }
+    session_start();
+
+    //Pricing module to calculate suggested price and total amount due
+    class PricingModule{
+        private $client;
+        private $clientHistory;
+        private $pricePerGallon = 1.50;
+        private $locationFactor;
+        private $rateHistoryFactor;
+        private $gallonsRequestedFactor;
+        private $companyProfitFactor;
+        public $gallonsRequested;
+        private $deliveryRequested;
+        private $deliveryAddress;
+        private $outOfState;
+    
+        private $test;
+    
+        public $companyProfitFactor = 0.10;
+    
+        public function setLocationFactor(){
+            $this->outOfState = ($this->client->state != "TX") ? true : false;
+            $this->locationFactor = ($this->outOfState) ? 0.04 : 0.02;
+        }
+    
+        public function setHistoryFactor(){
+            $this->rateHistoryFactor = (!empty($this->clientHistory)) ? 0.01 : 0;
+        }
+    
+        public function setGallonRequestedFactor(){
+            $this->gallonsRequestedFactor = (($this->gallonsRequested) > 1000) ? 0.02 : 0.03;
+        }
+    
+        public function margin(){
+            ($this->locationFactor - $this->rateHistoryFactor + $this->gallonsRequestedFactor + $this->companyProfitFactor) * $this->pricePerGallon;
+        }
+    
+        //suggested price = price per gallon + margin
+        public function suggestedPricePerGallon(){
+            return $this->pricePerGallon + $this->margin;
+        }
+    
+        public function totalAmountDue(){
+            return $this->gallonsRequested * $this->suggestedPricePerGallon;
+        }
+    }
+?>
+
 <html>
 
 <head>
@@ -6,10 +60,6 @@
 </head>
 
 <body>
-    <div class= "logoutLink">
-        <a href="logout.php">Logout</a>
-    </div>
-
     <form class="FormBody">
         <header>
             <div class="Header">
@@ -50,8 +100,8 @@
                 Total Amount Due:
                 <input type="text" id="txtTotal_due" name="txtTotal_due" value="" readonly>                
             </div>
-
             <div class="Buttons">
+                <button name="btnQuote">Get Quote</button>
                 <button name="btnSubmit">Submit</button>
                 <button name="btnReset" type="reset" value="submit">Reset</button>
 
