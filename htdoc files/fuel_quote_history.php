70f8b62cbb280cafbec9227fe1d7e5e006034ccc
fenil9shah <50537407+fenil9shah@users.noreply.github.com>
2020-07-10 00:47:00 -0500

    Update fuel_quote_history.php
    linked to datbase. fields comes from database.

--- htdoc files/fuel_quote_history.php
+++ htdoc files/fuel_quote_history.php
@@ -1,22 +1,19 @@
-<?php 
-        $con = mysqli_connect("localhost", "root", "", "test");
-        if(!$con)
-        {
-            die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
-        }
+<?php
+    /*session_start();
+    include_once 'processes/fuel_quote_controller.php';
+    
+    //init classes
+    $user = new User1();
+    $f_quote = new Fuel_quote();
 
-        session_start();
-        $user_check = $_SESSION['user'];
-    //    echo "<script type='text/javascript'>alert('$user_check');window.location = '../login.php';</script>";
-        $ses_sql = mysqli_query($con, "SELECT * FROM `users` WHERE email = '$user_check'");
-        $row = mysqli_fetch_array($ses_sql);
-        $loginSession = $row['email'];
-        if(empty($loginSession)){
-            header('location: login.php');
-           // echo "<script type='text/javascript'>alert('$loginSession');window.location = '../login.php';</script>";
+    $userID = $_SESSION['id'];
 
-        }
-
+    //catch method POST
+    //if($_SERVER["REQUEST_METHOD"]=="POST"){
+       // $f_quote->validation_inputs();
+    $f_quote->get_fuel_quote_history_by_user_id($userID);
+        
+    //}*/
 ?>
 
 
@@ -25,59 +22,51 @@
     
     <head>
         <title>Fuel Quote History</title>
-          <link rel="stylesheet" type="text/css" href="styles/fuel_quote_history.css" />
+          <link rel="stylesheet" href="styles/fuel_quote_history.css">
     </head>
     
     <body>
-
-        <div class= "logoutLink">
-            <a href="logout.php">Logout</a>
-        </div>
 
         <h1>Fuel Quote History</h1>
         <br>
         <table>
             <tr>
-                <th>Client Name</th>
+                <th>No.</th>
                 <th>Gallons Requested</th>
-                <th>Delivery Address</th>
-                <th>Delivery Date</th>
+                <th>Delivery Date</th>                
                 <th>Suggested Price / Gallon</th>
                 <th>Total Amount Due</th>
+                <th>Date Created</th>
             </tr>
-            <tr>
-                <td>Lester Harvey</td>
-                <td># of gallons requested</td>
-                <td>address</td>
-                <td>date</td>
-                <td>price</td>
-                <td>total cost</td>
-            </tr>
-            <tr>
-                <td>Fred Brown</td>
-                <td># of gallons requested</td>
-                <td>address</td>
-                <td>date</td>
-                <td>price</td>
-                <td>total cost</td>
-            </tr>
-            <tr>
-                <td>Charles Pearson</td>
-                <td># of gallons requested</td>
-                <td>address</td>
-                <td>date</td>
-                <td>price</td>
-                <td>total cost</td>
-            </tr>
-            <tr>
-                <td>Harry Brown</td>
-                <td># of gallons requested</td>
-                <td>address</td>
-                <td>date</td>
-                <td>price</td>
-                <td>total cost</td>
-            </tr>
-        </table>
+            <?php
+                session_start();
+                include_once 'processes/fuel_quote_controller.php';
+                
+                //init classes
+                $user = new User1();
+                $f_quote = new Fuel_quote();
+                $db = new DB();            
+                $userID = $_SESSION['id'];
+
+              
+                $query1 = mysqli_query($db->link(), "SELECT * FROM fuel_quotes WHERE userid = $userID");
+                if($query1->num_rows > 0){
+                    $i = 1;
+                    while($row = $query1->fetch_assoc()) {
+                        echo "<tr><td>" .$i. "</td><td>" . $row["gallons_requested"] . "</td><td>"
+                        . $row["delivery_date"]. "</td><td>" . $row["suggested_price"] . "</td><td>" . $row["total_amount_due"] . "</td><td>" . $row["created_date"] . "</td></tr>";
+                        $i = $i + 1;
+                    }
+                    echo "</table>";
+                } else { echo "0 results"; }
+                
+            ?>
+            
+        <br><br>
+        <div class="Buttons">
+        <button onclick="location.href='fuel_quote.php'"type="button">Generate New Quote</button>
+        <button onclick="location.href='logout.php'"type="button">Logout</button>
+        </div>
     </body>
 </html>
 
